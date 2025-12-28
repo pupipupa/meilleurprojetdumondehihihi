55ca4b00fd5ccee3f8cca9a3327657c4cb58194c
pupipupa <hwarushrimp@gmail.com>
2025-12-28 22:41:50 +0100

    67

--- II.c
+++ II.c
@@ -5,6 +5,12 @@
 const char path[50];
 printf("Le chemin/\n");
 scanf(%s, &path);
-FILE *f = fopen(path, "rb");
+
+
+int ouvrir_file(const char* path){
+    FILE *f = fopen(path, "rb");
+    if(f==NULL) return NULL;
+    
+}
 
 //TODO: fncts pour passer de file à texte code mais genre avec le bon registre sans la ponctuation et tout le reste
