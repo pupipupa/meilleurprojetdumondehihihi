97da8a91237747f3c8dbd4625bf24a9ed57438f3
pupipupa <hwarushrimp@gmail.com>
2025-12-28 22:36:52 +0100

    ca commence

---
+++ II.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+
+const char path[50];
+printf("Le chemin/\n");
+scanf(%s, &path);
+FILE *f = fopen(path, "rb");
+
+//TODO: fncts pour passer de file à texte code mais genre avec le bon registre sans la ponctuation et tout le reste
