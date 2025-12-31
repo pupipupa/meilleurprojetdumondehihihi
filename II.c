50ebb1ad525d8ebd0696e9dd3855e5b1f58cbdb0
pupipupa <hwarushrimp@gmail.com>
2025-12-31 08:53:11 +0100

    blob

--- II.c
+++ II.c
@@ -9,8 +9,15 @@ scanf(%s, &path);
 
 int ouvrir_file(const char* path){
     FILE *f = fopen(path, "rb");
-    if(f==NULL) return NULL;
+    if(f==NULL) return 0;
+    //TODO: créér un tableau des pointeurs sur le char 
+    //TODO: alors du coup il reste d'alluer de la mémoire 
+    //TODO: mettre dans cette mémoire les bytes
+
+    //NOTE; free?
     
+
+    fclose(&*(f));
 }
 
 //TODO: fncts pour passer de file à texte code mais genre avec le bon registre sans la ponctuation et tout le reste
