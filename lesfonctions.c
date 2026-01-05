8f4cd2f725b65a8c7369c8f2a9780de3018847d6
pupipupa <hwarushrimp@gmail.com>
2026-01-05 06:30:59 +0100

    RIENNN CHANGéEEEEEEEEEEEjuste chaque fichier dans son propre dossier home hihi

--- lesfonctions.c
+++
@@ -1,8 +0,0 @@
-//////////////////////////////////////////////////////////////////////////////
-// - II.C
-
-char* ouvrir_file(const char* path);
-int normalisation_texte(char *s);
-int switch_fr(unsigned char c);
-int normalisation_texte_v2(char *s);
-void printtestcontenu(char *content);
