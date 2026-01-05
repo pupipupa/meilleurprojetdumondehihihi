804971738ce689cb925ba504609ac816d550ed17
pupipupa <hwarushrimp@gmail.com>
2026-01-05 12:03:46 +0100

    des structures pour l'algo avec des arbres binaires

--- source/algo.c
+++ source/algo.c
@@ -17,3 +17,26 @@
 //!3
 
 //!4(?)
+
+//I WILL TRY 
+//ARBRE binaire: 
+
+typedef struct Noeud Noeud;
+
+struct {
+  Mot data;
+  Noeud* gauche;  
+  Noeud* droite;
+
+} Noeud;
+
+typedef struct {
+   Noeud* racine;
+   size_t nb_mots_uniques;
+  } Arbre;
+
+typedef struct{
+    Noeud** classement; 
+    int k;
+    int occupes;
+} TopK;
