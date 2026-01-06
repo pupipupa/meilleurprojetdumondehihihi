af49451bbf031690ffb93bc1a7ff13d94cfabddc
gearless-pim <dollaantoine06@gmail.com>
2026-01-06 14:56:53 +0100

    Mise à jour des fonctions mémoire ( le dlc à 15 balles que j'offre gratuitement au peuple, c'est pas pour rien qu'on vit dans la rue)

--- source/my_memoire.c
+++ source/my_memoire.c
@@ -16,6 +16,13 @@ void* myMalloc(size_t size, InfoMem* infoMem) //BUT : alloue une taille et met �
     void* p = malloc(size);
     if (p != NULL && infoMem != NULL) { //regarde si allocation réussi et que structure existe
         infoMem->cumul_alloc += size; //Met à jour le compteur des allocations faites
+
+        // Mémoire actuellement utilisée = alloc - désalloc
+        size_t mem_actuelle =  infoMem->cumul_alloc - infoMem->cumul_desalloc;
+
+        // Mise à jour du pic mémoire si nécessaire
+        if (mem_actuelle > infoMem->max_alloc) infoMem->max_alloc = mem_actuelle;
+        
     }
     return p; //NULL en cas d'échec
 }
@@ -31,6 +38,14 @@ void* myRealloc(void* ptr, size_t new_size, InfoMem* infoMem, size_t old_size) /
     if (p != NULL && infoMem != NULL) { // On ne sait pas si realloc a agrandi ou rapetissé la taille, alors on va incrémenter les deux valeurs
         infoMem->cumul_alloc    += new_size;
         infoMem->cumul_desalloc += old_size;
+
+        // Mémoire actuellement utilisée
+        size_t mem_actuelle = infoMem->cumul_alloc - infoMem->cumul_desalloc;
+
+        // idem myMalloc
+        if (mem_actuelle > infoMem->max_alloc)
+            infoMem->max_alloc = mem_actuelle;
+    
     }
     return p; // NULL en cas d’échec
 }
